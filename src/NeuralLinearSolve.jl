cb31baadc4e4e4ae88e081068c6c8b51aac8d5df
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-20 10:59:08 -0400

    Add representative PrecompileTools workload
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- src/NeuralLinearSolve.jl
+++ src/NeuralLinearSolve.jl
@@ -5,10 +5,19 @@ using Flux: Flux
 using Images: Images
 using JLD2: JLD2
 using NNlib: NNlib
+using PrecompileTools: @compile_workload, @setup_workload
 
 include("spy_plot.jl")
 include("predict.jl")
 
 export predict_solver, predict_solver_probs
+
+@setup_workload begin
+    @compile_workload begin
+        A = SparseArrays.sparse([1, 2, 3], [1, 2, 3], [1.0, 2.0, 3.0], 3, 3)
+        predict_solver(A)
+        predict_solver_probs(A)
+    end
+end
 
 end
